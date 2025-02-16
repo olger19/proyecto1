4b2ac28358e0cb3264a53b0c5ebfd757649234aa
OlgerQuispe <goldsag43@gmail.com>
2025-02-15 23:01:44 -0500

    update db password and install tailwindcss - frontend

--- backend/db.ts
+++ backend/db.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
+    password: String(process.env.DB_PASSWORD),
     port: Number(process.env.DB_PORT), // Este es puerto por defecto de postgres en .env
 })
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
+
 
 function App() {
-  const [data, setData] = useState<{ title: string; links: { text: string; href: string }[] } | null>(null);
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/api/scrape")
-      .then((res) => setData(res.data))
-      .catch((err) => console.error(err));
-  }, []);
+  const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1>{data?.title}</h1>
-      <ul>
-        {data?.links.map((link, index) => (
-          <li key={index}>
-            <a href={link.href} target="_blank" rel="noopener noreferrer">{link.text}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h2 className="text-2xl text-blue-500"> Hello World </h2>
+    </>
   );
 }
 
